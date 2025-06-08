71611f16f0b9bc9f4d5f4ac4a9c1744dc418b0eb
Vaibhav Jindal <jindalvaibhav63@gmail.com>
2025-06-08 22:28:31 +0530

    changed app file

--- app.py
+++ app.py
@@ -105,4 +105,5 @@ def dis_predict():
 
 # ==================== Run App ====================
 if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
